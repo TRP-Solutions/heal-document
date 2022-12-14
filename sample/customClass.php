4c4cc59992fef04c5c747f5ed5c1c6234749c6fa
Jacob Neergaard Pedersen <neergaard@kananga.dk>
2022-12-14 20:18:16 +0100

    Added customClass sample

---
+++ sample/customClass.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__.'/../lib/HealDocument.php';
+
+trait htmlDesignTrait {
+	// Added function
+	public function button($value, $onclick = null) {
+		$input = $this->el('input',['type'=>'button','value'=>$value]);
+		if($onclick) {
+			$input->at(['onclick'=>$onclick]);
+		}
+		return $input;
+	}
+
+	// Required precedence
+	protected static function createElementHeal($name){
+		return new htmlDesignElement($name);
+	}
+}
+
+class htmlDesign extends HealDocument {
+	use htmlDesignTrait;
+}
+
+class htmlDesignElement extends HealElement {
+	use htmlDesignTrait;
+}
+
+$doc = new htmlDesign();
+$doc->button('Test',"alert('click!');");
+echo $doc;
