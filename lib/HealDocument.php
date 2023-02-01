671eaebb7693c51222f311f06b82c38a33551adf
Zatnosk <zatnosk@zatnosk.dk>
2023-02-01 14:15:38 +0100

    Implemented plugin support

--- lib/HealDocument.php
+++ lib/HealDocument.php
@@ -5,14 +5,14 @@ https://github.com/TRP-Solutions/heal-document/blob/master/LICENSE.txt
 */
 
 trait HealNodeParent {
-	public function el($name, $attributes = [], $append = false){
+	public function el($name, $attributes = [], $append = false) : HealComponent {
 		$element = static::createElementHeal($name);
 		$this->appendChild($element);
 		$element->at($attributes,$append);
 		return $element;
 	}
 
-	public function te($str, $break_on_newline = false){
+	public function te($str, $break_on_newline = false) : HealComponent {
 		if($break_on_newline){
 			$lines = explode("\n",$str);
 			$firstline = true;
@@ -31,13 +31,13 @@ trait HealNodeParent {
 		return $this;
 	}
 
-	public function co($str){
+	public function co($str) : HealComponent {
 		$this->appendChild(new DOMComment($str));
 		// return $this to allow chaining
 		return $this;
 	}
 
-	public function fr($str){
+	public function fr($str) : bool {
 		$fragment = $this->ownerDocument->createDocumentFragment();
 		if(@$fragment->appendXML($str)) {
 			$this->appendChild($fragment);
@@ -49,13 +49,19 @@ trait HealNodeParent {
 		}
 	}
 
-	protected static function createElementHeal($name){
+	public function __call($name, $arguments){
+		return HealDocument::try_plugin($this, $name, $arguments);
+	}
+
+	protected static function createElementHeal($name) : HealComponent {
 		return new HealElement($name);
 	}
 }
 
 class HealDocument extends DOMDocument {
 	use HealNodeParent;
+
+	private static $plugins = [], $plugin_name_cache = [], $has_prefixed_plugins = false;
 
 	private $toStringFormat;
 
@@ -81,12 +87,59 @@ class HealDocument extends DOMDocument {
 			return $this->saveXML();
 		}
 	}
+
+	public static function register_plugin($classname, $prefix = null){
+		if(!class_exists($classname)){
+			throw new \Exception("HealDocument can't find class '$classname'");
+		}
+		if(!is_subclass_of($classname, 'HealPluginInterface')){
+			throw new \Exception("HealDocument can't register plugin '$classname', because it doesn't implement HealPluginInterface or extend the HealPlugin abstract class.");
+		}
+
+		if(isset($prefix)){
+			self::$has_prefixed_plugins = true;
+			self::$plugins[$prefix] = $classname;
+		} else {
+			self::$plugins[] = $classname;
+		}
+		if(!empty(self::$plugin_name_cache)){
+			self::$plugin_name_cache = [];
+		}
+	}
+
+	public static function try_plugin($parent, $fullname, $arguments){
+		if(isset(self::$plugin_name_cache[$fullname])){
+			return self::$plugin_name_cache[$fullname]::create($parent, $fullname, ...$arguments);
+		}
+		if(self::$has_prefixed_plugins){
+			$split_name = explode('_',$fullname,2);
+			if(count($split_name) == 2){
+				list($prefix,$name) = $split_name;
+			} else {
+				$prefix = null;
+				$name = $split_name[0];
+			}
+			if(!empty($prefix) && isset(self::$plugins[$prefix])){
+				$classname = self::$plugins[$prefix];
+				if($classname::can_create($name)){
+					self::$plugin_name_cache[$name] = $classname;
+					return $classname::create($parent, $name, ...$arguments);
+				}
+			}
+		}
+		foreach(self::$plugins as $plugin){
+			if($plugin::can_create($fullname)){
+				self::$plugin_name_cache[$fullname] = $plugin;
+				return $plugin::create($parent, $fullname, ...$arguments);
+			}
+		}
+	}
 }
 
-class HealElement extends DOMElement {
+class HealElement extends DOMElement implements HealComponent {
 	use HealNodeParent;
 
-	public function at($values, $append = false){
+	public function at($values, $append = false) : HealComponent {
 		foreach($values as $name => $value){
 			if(is_numeric($name)){
 				$attr = $this->ownerDocument->createAttribute($value);
@@ -110,5 +163,55 @@ class HealElement extends DOMElement {
 
 		// return $this to allow chaining
 		return $this;
+	}
+}
+
+interface HealComponent {
+	public function el($name, $attributes = [], $append = false) : HealComponent;
+	public function at($values, $append = false) : HealComponent;
+	public function te($str, $break_on_newline = false) : HealComponent;
+	public function co($str) : HealComponent;
+	public function fr($str) : bool;
+}
+
+interface HealPluginInterface {
+	public static function can_create($name) : bool;
+	public static function create($parent, $name, ...$arguments) : HealComponent;
+}
+
+abstract class HealPlugin implements HealPluginInterface, HealComponent {
+	public static function can_create($name) : bool{
+		return method_exists(static::class, $name);
+	}
+
+	public static function create($parent, $name, ...$arguments) : HealComponent {
+		if(method_exists(static::class, $name)){
+			$object = static::$name($parent, ...$arguments);
+			if(is_a($object, static::class) && !isset($object->primary_element)){
+				$object->primary_element = $parent;
+			}
+			return $object;
+		} else {
+			throw new \Exception("HealPlugin failed to find method '$name'");
+		}
+	}
+
+	protected $primary_element;
+
+	public function el($name, $attributes = [], $append = false) : HealComponent {
+		$this->primary_element->el($name, $attributes, $append);
+	}
+
+	public function at($values, $append = false) : HealComponent {
+		$this->primary_element->at($values, $append);
+	}
+	public function te($str, $break_on_newline = false) : HealComponent {
+		$this->primary_element->te($str, $break_on_newline);
+	}
+	public function co($str) : HealComponent {
+		$this->primary_element->co($str);
+	}
+	public function fr($str) : bool {
+		$this->primary_element->fr($str);
 	}
 }
---
+++ sample/plugin.php
@@ -0,0 +1,45 @@
+<?php
+/*
+HealDocument is licensed under the Apache License 2.0 license
+https://github.com/TRP-Solutions/heal-document/blob/master/LICENSE.txt
+*/
+require_once "../lib/HealDocument.php";
+
+class ExamplePlugin extends HealPlugin {
+	public static function labelled_input($parent, $label, $value, $name = null, $placeholder = null){
+		$div = $parent->el('div');
+		$label = $div->el('label')->te($label);
+		$input = $label->el('input',['type'=>'text','value'=>$value]);
+		if(isset($name)){
+			$input->at(['name'=>$name]);
+		}
+		if(isset($placeholder)){
+			$input->at(['placeholder'=>$placeholder]);
+		}
+		return $input;
+	}
+}
+
+HealDocument::register_plugin('ExamplePlugin');
+
+$doc = new HealDocument('html');
+$html = $doc->el('html',['lang'=>'en']);
+$head = $html->el('head');
+$head->el('meta',['charset'=>'utf-8']);
+$head->el('title')->te('Hello World');
+$body = $html->el('body');
+
+$head->el('style')
+	->te('body { width: 650px; border: 1px solid black; padding: 0 1em; box-sizing: border-box; }')
+	->te('h1, p { font-family: sans-serif; }')
+	->te('p { font-size: 1.2em; }');
+
+$body->el('h1')->te('Hello World');
+
+$body->el('p')->te('This is a simple example of how a plugins can be used. The following is an example plugin making labelled inputs.');
+
+$body->labelled_input('Input with value', 'Value');
+$body->labelled_input('Input with name', '', 'input_name');
+$body->labelled_input('Input with placeholder', '', null, 'Placeholder');
+
+echo $doc;
